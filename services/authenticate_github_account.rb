bc54845f2b732c5ee5a23ead56c9781c6fb0dff6
An-Ting Hsu <andyhsu10@gmail.com>
2018-06-21 02:22:52 +0800

    Github SSO bugfix

--- services/authenticate_github_account.rb
+++ services/authenticate_github_account.rb
@@ -8,7 +8,7 @@ class AuthenticateGithubAccount
     
     def call(code)
       access_token = get_access_token_from_github(code)
-      get_sso_account_from_api(access_token)
+      get_sso_gh_account_from_api(access_token)
     end
     
     private
@@ -17,14 +17,14 @@ class AuthenticateGithubAccount
       challenge_response =
         HTTP.headers(accept: 'application/json')
             .post(@config.GH_TOKEN_URL,
-                  form: { client_id: @config.GH_CLIENT_ID,
+                  json: { client_id: @config.GH_CLIENT_ID,
                           client_secret: @config.GH_CLIENT_SECRET,
                           code: code })
       raise unless challenge_response.status < 400
       challenge_response.parse['access_token']
     end
     
-    def get_sso_account_from_api(access_token)
+    def get_sso_gh_account_from_api(access_token)
       sso_info = { access_token: access_token }
       signed_sso_info = SecureMessage.sign(sso_info)
       
